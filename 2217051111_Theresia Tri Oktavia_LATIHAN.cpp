5eca07bd2ff4a445193b918f03f060f5164a6191
TheresiaTriOktavia <112594407+TheresiaTriOktavia@users.noreply.github.com>
2022-09-09 15:47:59 +0700

    Add files via upload

---
+++ 2217051111_Theresia Tri Oktavia_LATIHAN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float phi=3.14;
+	int r;
+	cout<<"Luas lingkaran\n";
+	cout<<"phi = "<<phi;
+	cout<<"\nr = ";
+	cin>>r;
+	cout<<"phi*r*r = "<<phi*r*r;
+	
+	return 0;
+}
